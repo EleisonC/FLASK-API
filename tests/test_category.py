3245fc2565175b30441f730b30aa6d0c4489e190
Mom <ckalule7@gmail.com>
2017-12-07 16:11:18 +0300

    add TDD for categories

--- tests/test_category.py
+++ tests/test_category.py
@@ -9,8 +9,8 @@ class CategorylistTestCase(unittest.TestCase):
     def setUp(self):
         """Define test variables and initialize app."""
         self.app = create_app(config_name='testing')
-        self.client = self.app.test_client
-        self.categorylist = {'name': 'Lunch'}
+        self.client = self.app.test_client()
+        self.categorylist = {'category_name': 'Lunch'}
 
         #binds the app to the current context
         with self.app.app_context():
@@ -19,25 +19,25 @@ class CategorylistTestCase(unittest.TestCase):
 
     def test_category_creation(self):
         """Test API can create a categorylist (POST request) """
-        res = self.client.post('/categorylist/', data=self.categorylist)
+        res = self.client.post('/category_creation/', data=self.categorylist)
         self.assertEqual(res.status_code, 201)
         self.assertIn('Lunch', str(res.data))
 
     def test_api_can_get_all_categories(self):
         """Test API can get a category (Get request)"""
-        res = self.client.post('/categorylist/', data=self.categorylist)
+        res = self.client.post('/category_creation/', data=self.categorylist)
         self.assertEqual(res.status_code, 201)
-        res = self.client.get('/categorylist/')
+        res = self.client.get('/category_view_all/')
         self.assertEqual(res.status_code, 200)
         self.assertIn('Lunch', str(res.data))
 
     def test_api_can_get_category_by_id(self):
         """Test API can get a single category by using """
-        rv = self.client.post('/categorylist/', data=self.categorylist)
+        rv = self.client.post('/category_creation/', data=self.categorylist)
         self.assertEqual(rv.status_code, 201)
         result_in_json = json.loads(rv.data.decode('utf-8').replace("'","\""))
         result = self.client.get(
-            '/categorylist/{}'.format(result_in_json['id'])
+            '/category_manipulation/{}'.format(result_in_json['id'])
         )
         self.assertEqual(result.status_code, 200)
         self.assertIn('Lunch', str(result.data))
@@ -45,34 +45,34 @@ class CategorylistTestCase(unittest.TestCase):
     def test_category_can_be_edited(self):
         """Test API can edit an existing category. (Put request)"""
         rv = self.client.post(
-            '/categorylist/',
-            data={'name': 'Breakfast'}
+            '/category_creation/',
+            data={'category_name': 'Breakfast'}
         )
         self.assertEqual(rv.status_code, 201)
         rv = self.client.put(
-            '/categorylist/1',  
-            data={'name': 'Soups'}
+            '/category_manipulation/1',  
+            data={'category_name': 'Soups'}
         )
         self.assertEqual(rv.status_code, 200)
-        results = self.client.get('/categorylist/1')
+        results = self.client.get('/category_manipulation/1')
         self.assertIn('Soups', str(results.data))
 
     def test_categorylist_deletion(self):
         """Test API can delete an exsisting category. (DELETE request)."""
         rv = self.client.post(
-            '/categorylist/',
-            data= {'name':'Dinner'}
+            '/category_creation/',
+            data= {'category_name':'Dinner'}
         )
         self.assertEqual(rv.status_code, 201)
-        res = self.client.delete('/categorylist/1')
+        res = self.client.delete('/category_manipulation/1')
         self.assertEqual(res.status_code, 200)
-        result = self.client.get('/categorylist/1')
+        result = self.client.get('/category_manipulation/1')
         self.assertEqual(result.status_code, 404)
 
     def tearDown(self):
         """teardown all initalized variables."""
         with self.app.app_context():
-            db,session.remove()
+            db.session.remove()
             db.drop_all()
 
 if __name__ == " __main__":
