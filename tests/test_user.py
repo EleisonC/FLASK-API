3a7f867474706ac644a0b3742ab3c89170193245
Mom <ckalule7@gmail.com>
2017-12-12 14:35:01 +0300

    modify test_user.py file

--- tests/test_user.py
+++ tests/test_user.py
@@ -1,7 +1,8 @@
 import unittest
 import os
 import json
-from app import create_app, db 
+from app import create_app, db
+
 
 class UserTestCase(unittest.TestCase):
     """testcases for the authentication Blueprint"""
@@ -12,67 +13,81 @@ class UserTestCase(unittest.TestCase):
         self.client = self.app.test_client()
         self.user_data = {
             'username': 'chris',
-            'password': '1234'
+            'password': 'Chris12'
         }
 
         with self.app.app_context():
-            #create all tables
+            # create all tables
             db.session.close()
             db.drop_all()
             db.create_all()
-        
+
+    def test_data_validation(self):
+        """ test validity of user data."""
+        userdata = {
+            "username": "james",
+            "password": "chris"
+        }
+        res = self.client.post('/auth/register', data=userdata)
+        # to get the results returned in json format
+        result = json.loads(res.data.decode())
+        # assert that the request contains a success
+        self.assertEqual(result["message"],
+                         'Invalid username or password. Please try again.')
+        self.assertEqual(res.status_code, 403)
 
     def test_registration(self):
         """test user registration works"""
         res = self.client.post('/auth/register', data=self.user_data)
         # to get the results returned in json format
         result = json.loads(res.data.decode())
-        #assert that the request contains a success message and a 201 status code
-        self.assertEqual(result['message'], 'You registered successfully. Please log in.')
+        # assert that the request contains a success message and a 201 status code
+        self.assertEqual(result['message'],
+                         'You registered successfully. Please log in.')
         self.assertEqual(res.status_code, 201)
 
     def test_already_registered_user(self):
         """Test that a user cannot be registered twice"""
         res = self.client.post('/auth/register', data=self.user_data)
-        
+
         # self.assertEqual(res.status_code, 201)
         second_res = self.client.post('/auth/register', data=self.user_data)
         self.assertEqual(second_res.status_code, 202)
-        #get the results returned in json format
+        # get the results returned in json format
         result = json.loads(second_res.data.decode())
         self.assertEqual(
             result['message'], 'User already exists. Please login'
         )
+
     def test_user_login(self):
         """Test registered user can login"""
         res = self.client.post('/auth/register', data=self.user_data)
-        self.assertEqual(res.status_code,201)
-        login_res = self.client.post('/auth/login',data=self.user_data)
+        self.assertEqual(res.status_code, 201)
+        login_res = self.client.post('/auth/login', data=self.user_data)
 
-        #get results in json format
+        # get results in json format
         result = json.loads(login_res.data.decode())
-        #test that the response contains succes message
+        # test that the response contains succes message
         self.assertEqual(result['message'], 'You logged in successfully')
-        #assert that the status code is equal to 200 
+        # assert that the status code is equal to 200
         self.assertEqual(login_res.status_code, 200)
         self.assertTrue(result['access_token'])
 
     def test_non_registered_user_login(self):
         """Test non registered users cannot login"""
-        #define a dictionary to represent an unregistered user
+        # define a dictionary to represent an unregistered user
         not_a_user = {
-            'username' : 'unknown',
-            'password' : 'not_available'
+            'username': 'unknown',
+            'password': 'not_available'
         }
-        #send a POST request to /auth/login with the data above
+        # send a POST request to /auth/login with the data above
         res = self.client.post('/auth/login', data=not_a_user)
-        #get the result in json
+        # get the result in json
         result = json.loads(res.data.decode())
 
-        #assert that this response must contain an error message 
-        #and an error statuscode 401(Unauthorized)
+        # assert that this response must contain an error message
+        # and an error statuscode 401(Unauthorized)
         self.assertEqual(res.status_code, 401)
         self.assertEqual(
             result['message'], 'Invalid username or password, please try again'
         )
-
