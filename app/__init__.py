986c583a812d4df9f9e5f845220049adaf82f580
christopher kalule <christopherkalule@christophers-MacBook-Pro.local>
2018-01-19 16:39:38 +0300

    modify init.py file

--- app/__init__.py
+++ app/__init__.py
@@ -2,17 +2,9 @@ from flask_api import FlaskAPI
 from flask_sqlalchemy import SQLAlchemy
 # this is a local import
 from instance.config import app_config
-"""POSTGRES = {
-    'user': 'postgres',
-    'pw': 'chrisenlarry',
-    'db': 'yummys',
-    'host': 'localhost',
-    'port': '5432'
-}"""
+
 # initialize sql-alchemy
-
 db = SQLAlchemy()
-
 
 def create_app(config_name):
     app = FlaskAPI(__name__, instance_relative_config=True)
